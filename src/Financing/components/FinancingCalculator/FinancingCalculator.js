08e19105ceb369a09afe8ace20262ef399163970
RXBVK <52883785+RXBVK@users.noreply.github.com>
2021-05-29 15:23:34 +0200

    Add financing/calculator, formula and additional styling to fix

--- src/Financing/components/FinancingCalculator/FinancingCalculator.js
+++ src/Financing/components/FinancingCalculator/FinancingCalculator.js
@@ -1,9 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function FinancingCalculator() {
+  const [desiredVehiclePrice, setDesiredVehiclePrice] = useState(0);
+  const [salesTax, setSalesTax] = useState(6.25);
+  const [termInMonths, setTermInMonths] = useState(144);
+  const [rateOrAPR, setRateOrAPR] = useState(7);
+  const [downPayment, setDownPayment] = useState(0);
+
+  let properVehiclePrice =
+    downPayment > desiredVehiclePrice ? 0 : desiredVehiclePrice - downPayment;
+  let afterTaxation =
+    properVehiclePrice + (properVehiclePrice / 100) * salesTax;
+  let afterAPR = afterTaxation + (afterTaxation / 100) * rateOrAPR;
+  let monthlyPayment = afterAPR / termInMonths;
+
   return (
     <div className="financing__calculator">
-      <h1>Calculator</h1>
+      <h1>Financial Calculator</h1>
+      <div className="financing__calculator-content">
+        <div className="financing__calculator-content-form">
+          <h3>Calculate Payment</h3>
+          <label>Desired Vehicle Price</label>
+          <input
+            type="number"
+            value={desiredVehiclePrice}
+            onChange={(e) => setDesiredVehiclePrice(e.target.value)}
+          />
+          <label>Sales Tax</label>
+          <input
+            type="number"
+            value={salesTax}
+            onChange={(e) => setSalesTax(e.target.value)}
+          />
+          <label>Term in Months</label>
+          <input
+            type="number"
+            value={termInMonths}
+            onChange={(e) => setTermInMonths(e.target.value)}
+          />
+          <label>Rate / APR</label>
+          <input
+            type="number"
+            value={rateOrAPR}
+            onChange={(e) => setRateOrAPR(e.target.value)}
+          />
+          <label>Down Payment or Trade-In Value</label>
+          <input
+            type="number"
+            value={downPayment}
+            onChange={(e) => setDownPayment(e.target.value)}
+          />
+        </div>
+        <div className="financing__calculator-content-output">
+          <h3>Estimated Monthly Payment</h3>
+          <div className="financing__calculator-content-output-circle">
+            <div>{`$${monthlyPayment.toFixed(2)}`}</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
